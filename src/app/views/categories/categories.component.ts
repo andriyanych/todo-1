c5de8f5e0f6b6041bc378b82a3da10dc97792497
usr112 <andriyanovsv@gmail.com>
2021-05-02 00:28:04 +0800

    добавили отображение задач по категории, пока только в консоли

--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -21,4 +21,7 @@ export class CategoriesComponent implements OnInit {
         console.log(this.categories);
     }
 
+    showTasksByCategory(category: Category) {
+        this.dataHandler.getTasksByCategory(category);
+    }
 }
